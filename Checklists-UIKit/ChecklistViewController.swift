fdc7aad3e55170909094efa4363f8a66c2982c21
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-13 06:06:18 -0700

    Detailed instructions for addItem() method.

--- Checklists-UIKit/ChecklistViewController.swift
+++ Checklists-UIKit/ChecklistViewController.swift
@@ -95,5 +95,20 @@ class ChecklistViewController: UITableViewController {
         label.text = item.text
         
     }
+    
+    //    MARK: - Actions
+    
+    @IBAction func addItem() {
+        
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
 
 }
