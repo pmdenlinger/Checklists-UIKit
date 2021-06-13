50454cc92f989d1ac2d2209f31928e5da1103c09
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-13 04:59:36 -0700

    Create ChecklistItem object with default properties.

---
+++ Checklists-UIKit/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists-UIKit
+//
+//  Created by user on 6/13/21.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
