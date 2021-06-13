2b53d5ec2234da227f26dda02b5c0b5b4264595a
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-13 10:35:12 -0700

    Add UITextFieldDelegate, doneBarButton outlet

--- Checklists-UIKit/AddItemViewControllerTableViewController.swift
+++ Checklists-UIKit/AddItemViewControllerTableViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 
-class AddItemViewControllerTableViewController: UITableViewController {
+class AddItemViewControllerTableViewController: UITableViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +41,16 @@ class AddItemViewControllerTableViewController: UITableViewController {
         
         print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
-        
-        
     }
-}
+//    MARK: - Text Field Delegates
+    
+    func textField( _ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    }
+
