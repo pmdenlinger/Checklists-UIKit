91b9a7f97811100100d23b0fc6504e2706443e91
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-13 06:35:22 -0700

    Add AddItemViewController, change title display mode to never.

---
+++ Checklists-UIKit/AddItemViewControllerTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddItemViewControllerTableViewController.swift
+//  Checklists-UIKit
+//
+//  Created by user on 6/13/21.
+//
+
+import UIKit
+
+class AddItemViewControllerTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+
+        
+    }
+}
